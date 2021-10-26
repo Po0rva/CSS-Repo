1291663b75197afe52fe48f8dbcba887d4c9b67c
poorva <poorvak279@gmail.com>
2021-10-26 12:14:46 +0530

    primitive data types

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+console.warn("Hello warning");
+console.error("Hello error"); 
+
+//Primitive data types//
+//string
+console.log(typeof "Hello world")
+console.log(typeof 'd')
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof -1.256488)
+console.log(typeof 125.6488)
+
+//Boolean
+console.log(typeof true)
+console.log(typeof false)
