52b8a129825d41f6e601fd92863a2a96909f04fc
poorva <poorvak279@gmail.com>
2021-10-27 23:26:40 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-console.log("Hello World");
-console.warn("Hello warning");
-console.error("Hello error"); 
+var age = null
 
-//Primitive data types//
-//string
-console.log(typeof "Hello world")
-console.log(typeof 'd')
-
-//Numbers
-console.log(typeof 1)
-console.log(typeof -1.256488)
-console.log(typeof 125.6488)
-
-//Boolean
-console.log(typeof true)
-console.log(typeof false)
+//console.log(Name)
+console.log(typeof age)
