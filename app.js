da2e8ef31755d8e64bbc1a0fbec60330e6fe0ac7
poorva <poorvak279@gmail.com>
2021-10-29 21:45:54 +0530

    operators

--- app.js
+++ app.js
@@ -1,4 +1,59 @@
-var age = null
+//operators//
+//Arithmetic operators//
 
-//console.log(Name)
-console.log(typeof age)
+var a = 2
+var b = 10
+
+console.log('a + b =', a + b)
+console.log('b - a =', b - a)
+console.log('b / a =', b / a)
+console.log('b * a =', b * a)
+console.log('b ** a =', b ** a)
+console.log('b % a =', b % a)
+
+var c = 10
+
+console.warn(c++)
+console.log(c)
+
+//Assignment operators//
+
+b = 10
+console.log('b',b)
+b += 10
+console.log('b+=1',b)
+b++
+console.log('b+=1',b)
+b -= 10
+console.log('b-=1',b)
+b --
+console.log('b--',b)
+
+//comparision operators//
+
+var d = 10
+var e = 2
+var f = '10'
+
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+
+console.warn(d == f)
+console.warn(d === f)
+
+//logical operators//
+
+console.log('true && true', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true', true || true)
+console.log('true || false', true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
+
+
