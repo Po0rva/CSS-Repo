f2438bd60574ea736e6177b2f31c28498eee81ad
poorva <poorvak279@gmail.com>
2021-10-30 14:59:55 +0530

    dark

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 var heading = document.getElementById('heading')
+var body = document.querySelector('body')
+
 heading.innerHTML = "hi from js"
 heading.style = "color:red; font-size:20px;"
 
 heading.classList.add('five')
 heading.classList.remove('one')
 
-console.log(heading.classList)
-
- 
-var heading = document.getElementById('heading')
- var para = document.getElementsByClassName('para')
- var tags = document.getElementsByTagName('a')
- var query = document.querySelector('.para')
- //var query = document.querySelector('#heading')
- var query = document.querySelectorAll('.para')
- 
- console.log(query)
-
-
- console.log(para.length)
- console.log(para[0])
- console.log(para[1])
- console.log(heading)
+body.classList.add('dark')
+body.classList.remove('dark')
 
  
