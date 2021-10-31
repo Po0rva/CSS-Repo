8f277ffc1df22c26f8be6354e06a71da66876eb7
poorva <poorvak279@gmail.com>
2021-10-31 21:47:18 +0530

    fun-1

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-//function greet(Name, age)
-//function greet(Name = 'chinni', age = 50)
-//{
-  //  console.log(Name, age)
-    //console.log('Hello')
-    //console.log('Hey ' + Name + "  your age is " + age)
-//}
-//greet('poorva','20')
-//greet()
 
-//function add(a = 0, b = 0)
+// shortcut for comment lines ctrl+k+c
+// for uncomment ctrl+k+u
+
+
+var arr = ['go to the gym','eat food','clean house']
+
+//for(var i=0; i< arr.length; i++)
 //{
-    //console.log(a + b)
-    //var sum = a + b
-   // return sum
+  //  console.log(arr[i])
 //}
 
-var add = function(a = 0, b = 0)
+// const greet = age => 'hello ' + age
+
+// console.log(greet('poorva'))
+//var callbackFunction = () =>
+var callbackFunction = (element)=>
 {
-    
-    var sum = a + b
-    return sum
+    //console.log('I ran')
+    console.log(element)
 }
+// arr.forEach(element, index)=>
+// {
+//     console.log(element, index)
+// }
 
-//console.log(add(10, 20))
-//add(10, 20)
-//var result = add(10, 20)
-//console.log(result)
+
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
+
+const callbackFunction = () =>
+{
+    console.log('sdf')
+}
+button.addEventListener('click', callbackfunc)
