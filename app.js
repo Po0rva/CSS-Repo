baa9212b34f3975a947dfbd99b89c5d4cc01a596
poorva <poorvak279@gmail.com>
2021-11-01 16:13:15 +0530

    functions

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
-
-// shortcut for comment lines ctrl+k+c
-// for uncomment ctrl+k+u
-
-
-var arr = ['go to the gym','eat food','clean house']
-
-//for(var i=0; i< arr.length; i++)
-//{
-  //  console.log(arr[i])
-//}
-
-// const greet = age => 'hello ' + age
-
-// console.log(greet('poorva'))
-//var callbackFunction = () =>
-var callbackFunction = (element)=>
-{
-    //console.log('I ran')
-    console.log(element)
-}
-// arr.forEach(element, index)=>
-// {
-//     console.log(element, index)
-// }
-
-
 var button = document.querySelector('button')
 var input = document.querySelector('input')
 var list = document.querySelector('ul')
 
-const callbackFunction = () =>
+const callbackfunc = (event) =>
 {
-    console.log('sdf')
+    //console.log(event.target)
+    //console.log(input.value)
+    //console.log(button.innerHTML)
+
+const inputValue = input.value
+const element = document.createElement('li')
+const textNode = document.createTextNode('inputValue')
+element.appendChild(textNode)
+list.appendChild(element)
 }
+
 button.addEventListener('click', callbackfunc)
