afd834f7b3cff4b037792bc5f449b4e6e7d5fcfe
poorva <poorvak279@gmail.com>
2021-11-03 09:20:04 +0530

    fun-1

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
+var body = document.querySelector('body')
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
 
-const callbackfunc = (event) =>
+const callbackfunc = (Event) => 
 {
-    //console.log(event.target)
-    //console.log(input.value)
-    //console.log(button.innerHTML)
-
-const inputValue = input.value
-const element = document.createElement('li')
-const textNode = document.createTextNode('inputValue')
-element.appendChild(textNode)
-list.appendChild(element)
+    //body.classList.add('dark')
+    body.classList.toggle('dark')
 }
 
 button.addEventListener('click', callbackfunc)
