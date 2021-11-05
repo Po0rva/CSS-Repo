3c16572609862e0ccb5151d927c7370cb69b7e3f
poorva <poorvak279@gmail.com>
2021-11-05 19:22:07 +0530

    array methods

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8] 
 
-//arr.push(6) //it adds element ending of an array.
-//arr.unshift(6)  //it adds element starting of an array.
 
-//arr.shift()  //it removes the 1st element of an array.
+console.log(arr.splice(3, 2))
 
-//var new_arr = arr.slice(4, 8)
-
-//br
-
-// var new_arr=[]
-
-// for(var i=0; i< arr.length; i++){
-//     if (arr[i] % 2 ==0) new_arr.push(arr[i])
-// }
-
-//br//
-
-var new_arr = arr.filter((element, index) => {
-    //console.log(element, index)
-    if(element < 100) return true
-})
-
-console.log(new_arr)
+console.log(arr)
