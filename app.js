4eb42185b7ddea010b4d92fd7bc79c42aa421eea
poorva <poorvak279@gmail.com>
2021-11-17 14:32:02 +0530

    objects & scopes

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8] 
+// let a = 20
+// var b = 40
+// console.log(a, b)
 
 
-console.log(arr.splice(3, 2))
+// if (true){
+//     var Name = 'Ferrari'
+// }
+// console.log(Name)
 
-console.log(arr)
+
+// const print = ()=>{
+//     var Name = "mercedes"
+//     if (true){
+//         console.log(Name)
+//     }
+//     return Name
+// }
+// print()
+
+
+
+//console.log(this.alert('hi'))
+//console.log(this)   refers to self window or else the parent object.
+
+
+// let car = {
+//     name: "C class",
+//     manufacturer: "mercedes",
+//     print:function (a){
+//         console.log(a)
+//     }
+// }
+// car.print("Ferrari")
+// console.log(car.name)
+// console.log(car.manufacturer)
+
+
+// let car = {
+//     name: "C class",
+//     manufacturer: "mercedes",
+//     print:function (a){
+//         console.log(`${this.name} was created by ${this.manufacturer}`) 
+//     }
+// }
+
+// car.print()
+
+
+// setTimeout(() => {
+// console.log("Bhavani")
+// }, 0)
+// console.log("HI")
+
+var data = fetch('https://kea.kar.nic.in/')
+console.log(data)
