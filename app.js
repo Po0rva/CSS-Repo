f28e87258e943f6d3b8cfdd9cdf37e5f6d67bf96
poorva <poorvak279@gmail.com>
2021-12-13 09:53:24 +0530

    revision of then & promise

--- app.js
+++ app.js
@@ -1,81 +1,41 @@
-//promises//
-//resolve = once if the promise is complete it will call the resolve function.
-//reject = once if the promise is incomplete it will call the reject function.
-//if the server return the promise completely it calls the resolve function.
-//if the server fails to give the data then it calls the reject function.
-// whenever the promise resolves it returns something.
-
-//promise has 3 states//
-// 1. fullfill state
-// 2. pending state
-// 3. rejected state
-// 4. setteled state
 
 
-let Name = new Promise((resolve,reject)=>{
- setTimeout(()=>{
-      resolve('poorva')
-     //reject('Hello')
- }, 3000)
+//Revision of js//
+
+//setTimeout = it creates the timer for callback 
+//function. once the timer completes it takes
+//two parameters a callback function and timer
+//as soon as the timer runs out it will execute the function.
+
+let greet = new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+       resolve("welcome")
+      }, 5000)
 })
+console.log(greet)
 
-Name.then(name=> {console.log(name)})
-// console.log(Name)
+//to get the value out of this greet//
 // setTimeout(()=>{
-//     console.log(Name)
-// }, 4000)
+//      console.log(greet)
+// },6000)  
+//instead of using the above setTimeout again
+// will be using (.then) to resolve
 
+// greet.then(value =>{
+//     console.log(value)
+//     return "welcome Again"
 
-// let response = fetch('https://jsonplaceholder.typicode.com/users')
-// .then(data => {
-//     console.log(data)
+    
+// }).then(newValue=>{
+//     console.log(newValue)
 // })
 
-//.then always returns promise//
-//.then itself is a new promise//
-// parseddata itself an promise//
 
-//  let response = fetch('https://jsonplaceholder.typicode.com/users')
-//  .then(res => res.json()).then(data => {
-//      console.log(data)
-//  })
+let response = fetch('https://jsonplaceholder.typicode.com/todos/10')
 
- // .json => .json converts the stream of data into some readable objects or
- // its just a collection of packets
- // Heap => Heap is a data structure , use heap to store data.
-
-
- 
-// const list = document.querySelector('ul')
-
-//  let arr = ['One','Two','Three','Four']
-
-// console.log(list)
-// arr.forEach(chore=>{
-// const element = document.createElement('li')
-// const textNode = document.createTextNode(chore)
-// element.appendChild(textNode)
-
-// element.addEventListener('click', (e) =>{
-//     const useInput = window.prompt('Are you sure that you want to delete $(chore)')
-//     if (useInput === 'yes') e.target.remove()
-// })
-
-// list.appendChild(element)
-// })
-
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const getData = async ()=> {
-    const url = 'https://api.edaman.com/search?app_id=bd5145f&app_key=3b35932Be30cad3141319969dfedaba9&q=Pizza'
-
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-}
-
-//add event listener to the button
-button.addEventListener('click', () => {
-     console.log(input.value)
+response.then(data =>{
+    return (data.json())
+}).then(content => {
+    console.log(content)
 })
 
