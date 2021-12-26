696f3240475c71bfe4e52563cb3622975225af29
poorva <poorvak279@gmail.com>
2021-12-26 16:33:43 +0530

    recipe card new

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-// const ul = document.querySelector('ul')
-// const but = document.querySelector('button')
-// const getText = document.querySelector('input')
-// const parent = document.querySelector('div.grid')
-// const getData = async (item) => {
-//     console.log(item)
-//     const url =  `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-//     console.log(url)
-//     const res = await fetch(url)
-//     const data = await res.json()
-//     const recipes = data.hits
-//     console.log(data.hits)
-//     recipes.forEach(item => {
-//         console.log(item)
-//     });
-// }
+const ul = document.querySelector('ul')
+const but = document.querySelector('button')
+const getText = document.querySelector('input')
+const parent = document.querySelector('div.grid')
 
-// //add event listener to the button
-// but.addEventListener('click', (e) =>{
-//     getData(getText.value)
-// })
+const getData = async (item) => {
+    console.log(item)
+    const url =  `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    const recipes = data.hits
+    console.log(data.hits)
+    recipes.forEach(item => {
+        console.log(item)
+        const template =`
+                         
+                     <img class="card__image" src="https://i.ibb.co/RT0bjJq/food1.png" />
+                     <div class="card__data">
+                        <div class="card__info">
+                           <h2>Nombre Comida</h2>
+                           <p>Descripcion de la comida, con ingredientes</p>
+                        </div>
+                        <h3 class="card__price">$7.50</h3>
+                        <button class="card__add">+</button>
+                     </div>
+                     `
+                    
+       const newCard = document.createElement('article')
+       newCard.setAttribute('class', 'card')
+       newCard.innerHTML = template
+       console.log(newCard)
+       parent.appendChild(newCard)              
+    })
+}
 
-
-
-const person = ({
-    name: 'poorva',
-    age: 10,
-    country: 'India'
+//add event listener to the button
+but.addEventListener('click', (e) =>{
+    getData(getText.value)
 })
 
-const { name, age, country} = person
 
-console.log( nName, age, country)
+
+// const person = ({
+//     name: 'poorva',
+//     age: 10,
+//     country: 'India'
+// })
+
+// const { name, age, country} = person
+
+// console.log( name, age, country)
